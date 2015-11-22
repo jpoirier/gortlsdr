40c8fbbbb72a56e1ba7dded82d5b14354f931e91
Joseph Poirier <jdpoirier@gmail.com>
2015-11-21 19:23:31 -0600

    fix the async callback so it's backward compat

--- rtlsdr.go
+++ rtlsdr.go
@@ -34,10 +34,10 @@ static inline rtlsdr_read_async_cb_t get_go_cb() {
 import "C"
 
 // Current version.
-var PackageVersion = "v2.9.6"
+var PackageVersion = "v2.9.7"
 
 // ReadAsyncCbT defines a user callback function type.
-type ReadAsyncCbT func([]byte)
+type ReadAsyncCbT func([]byte, _ *UserCtx)
 
 var clientCb ReadAsyncCbT
 
